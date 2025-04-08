5dedbce74dbf9c57e029a8cdba805afb404545da
Pproblems95 <l21212001@tectijuana.edu.mx>
2025-04-08 03:18:34 -0700

    se agregaron nuevos endpoints para inventario con logica que simula las operaciones y se empezó a usar postman para su depuración

--- src/controllers/inventarioController.ts
+++ src/controllers/inventarioController.ts
@@ -9,3 +9,18 @@ export const getInventario = (_req: Request, res: Response) => {
     console.log('Aqui se van a desplegar los productos del inventario para cada usuario')
     res.json(inventario)
 }
+
+export const postInventario = (_req: Request, res: Response) => {
+    console.log("El post va aqui")
+    inventario.push("Nuevo elemento")
+    res.json(inventario)
+}
+
+export const putInventario = (req: Request, res: Response) => {
+    const {id, producto}  = req.body;
+    inventario[id] = producto;
+    console.log("Aqui va el metodo put para cambiar el id")
+    res.json(inventario)
+    
+
+}
--- src/routes/inventory.ts
+++ src/routes/inventory.ts
@@ -1,8 +1,10 @@
 import express from 'express'
-import { getInventario } from '../controllers/inventarioController'
+import { getInventario, postInventario, putInventario } from '../controllers/inventarioController'
 
 const router = express.Router()
 
 router.get('/', getInventario)
+router.post('/', postInventario)
+router.put('/', putInventario)
 
 export default router
