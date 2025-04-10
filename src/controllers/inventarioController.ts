e46483c5428b679e7a4c62eb5ed7b61335e6eed8
Pproblems95 <l21212001@tectijuana.edu.mx>
2025-04-10 02:00:06 -0700

    se hizo el endpoint put en inventario, sin embargo, es necesario hacer los endpoints signup y signin para aplicar jwt y realizar las validaciones correspondientes en cada uno de los demas endpoints

--- src/controllers/inventarioController.ts
+++ src/controllers/inventarioController.ts
@@ -2,6 +2,7 @@
 import { Request, Response } from 'express'
 import prisma from '../prisma'
 
+
 const inventario: string[] = [
     "Leche", "Huevos", "Pan"
 ]
@@ -39,11 +40,26 @@ export const postInventario = async (req: Request, res: Response): Promise<void>
     }
 }
 
-export const putInventario = (req: Request, res: Response) => {
-    const {id, producto}  = req.body;
-    inventario[id] = producto;
-    console.log("Aqui va el metodo put para cambiar el id")
-    res.json(inventario)
+export const putInventario = async (req: Request, res: Response) => {
+    try{
+        const {id, nombre, descripcion, cantidad_actual, cantidad_minima, usuario_id} = req.body
+
+        const productoModificado = await prisma.inventario.update({
+            where: {id: id},
+            data:{
+                nombre,
+                descripcion,
+                cantidad_actual,
+                cantidad_minima,
+                usuario_id
+            }
+        })
+        res.status(200).json(productoModificado)
+    }
+    catch (error: any){
+        console.error("Error al editar elemento" + error)
+        res.status(500).json({message: "Error al modificar elemento", error: error.message})
+    }
 }
 
 export const deleteInventario = (req: Request, res: Response) => {
